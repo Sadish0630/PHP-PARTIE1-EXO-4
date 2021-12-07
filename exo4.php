97918f0e3815b1ea08fcc33560488b7df930a3d6
audemar <audemarsadish@gmail.com>
2021-12-07 15:19:34 +0100

    exo4 terminé

---
+++ exo4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 4</title>
+</head>
+<body>
+    <p>
+   <?php
+   $name = 'Sadish';
+   echo $name ;
+   ?>
+   </p>
+   <p>
+   <?php
+    $age = '40';
+   echo $age ;
+   ?>
+   </p>
+   <p>
+   <?php
+   $height = '1,66';
+   echo $height ;
+   ?>
+   </p>
+   <p>
+   <?php
+   $a = '1';
+   echo $a ;
+   ?>
+   </p>
+
+</body>
+</html>
